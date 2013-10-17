ed3459c7884225a52e2469e6da2816a5ca7956fd
Drew Shepard <dwshepard.dev@gmail.com>
2013-10-16 21:24:15 -0700

    my first commit

--- iostest/ViewController.m
+++ iostest/ViewController.m
@@ -18,6 +18,7 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    // I'm entering text here to test a commit to GitHub
 }
 
 - (void)didReceiveMemoryWarning
